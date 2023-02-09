5f0d761c40d8bff153b7c61a9f4c34d21b306aed
hishabee <maruf@hishabee.io>
2023-02-09 11:32:12 +0600

    add button component

--- src/Components/common/Button.vue
+++ src/Components/common/Button.vue
@@ -1,15 +1,25 @@
-<script>
-export default{
-
-};
-</script>
-
 <template>
   <div>
-    <button type="button">
-      
+    <button
+      :type="type"
+      @click="actions"
+      class="justify-self-center py-1.5 sm:py-3 bg-green-400 w-1/2 text-gray-600 text-md rounded-full shadow-md hover:scale-105 duration-300 focus:ring-1 hover:ring-1 ring-offset-1"
+    >
+      Add Post
     </button>
   </div>
 </template>
-
-<style></style>
+<script>
+export default {
+  props: {
+    actions: {
+      type: Function,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
